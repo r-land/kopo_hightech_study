055ad52b5008c0ad93187a2506ae861e966dd93d
r-land <labofsamplea@gmail.com>
2023-04-21 14:59:46 +0900

    edit mybatis

--- exweb/src/main/java/com/exweb/member/MemberVo.java
+++ exweb/src/main/java/com/exweb/member/MemberVo.java
@@ -4,7 +4,7 @@ package com.exweb.member;
 //이 클래스는 회원정보 한명의 정보를 모두 담고싶어서 만들었음
 //자바 캡슐화 접근제어자 프라이빗 붙여서 직접 접근 막기
 public class MemberVo {
-	private String memId;
+	private String memId; //mem_id 컬럼명이랑 다른 문제 자바는 기본이 나파표기법 별명으로 처리 as 띄어쓰기 있으면
 	private String memPass;
 	private String memName;
 	private int memPoint;
